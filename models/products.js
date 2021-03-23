b362e7ffaacc166138394dd17dbb3b8e7814cc1d
jamieroszel <jamie.s.roszel@gmail.com>
2021-03-23 16:20:21 -0400

    update products.js

--- models/products.js
+++ models/products.js
@@ -1,3 +1,11 @@
+// the process below has two steps: 
+// make our schema, which defines the structure
+// then create a model based on that schema
+// 'Product' defines the name of the model, which is a singular version of the collection name
+// that name is stored inside the constant Blog
+// then, export the module
+
+// create schema
 const Schema = mongoose.Schema;
 
 const productSchema = new Schema({
@@ -7,3 +15,8 @@ const productSchema = new Schema({
     price: {type: Number, min: 0},
     qty: {type: Number, min: 0},
 })
+
+// create model
+const Product = mongoose.model('Product', productSchema);
+
+module.export = Product;
